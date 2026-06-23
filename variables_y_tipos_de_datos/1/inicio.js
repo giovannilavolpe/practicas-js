2501dfa3d60b81198122ea1ac39da9389ada7f16
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-23 12:56:20 -0300

    feat(js) add variables practice 1

---
+++ variables_y_tipos_de_datos/1/inicio.js
@@ -0,0 +1,39 @@
+/*
+PARTE A
+
+typeof "Rosario" pienso que devuelve un string
+typeof 42 pienso que devuelve un number
+typeof 3.14 pienso que devuelve un number
+typeof true pienso que devuelve un boolean
+typeof false pienso que devuelve un boolean
+typeof undefined pienso que devuelve indefinido
+typeof null pienso que devuelve object ya que conozco el famoso bug.
+
+no me sorprendio ninguna.
+*/
+
+
+
+
+
+/* PARTE B, no me soprendio ninguna. */
+
+const ciudad    = "Mendoza"; 
+const habitantes = 115000;
+const esCap     = false;
+const apodo     = null;
+let fundacion;           // sin asignar
+
+console.log(typeof ciudad); /* pienso que devuelve un string */
+console.log(typeof habitantes); /* pienso que devuelve un number */
+console.log(typeof esCap); /* pienso que devuelve un boolean */
+console.log(typeof apodo); /* pienso que devuelve un object */
+console.log(typeof fundacion); /* pienso que devuelve undefined */
+
+
+
+
+// PARTE C --- la verdad, no se que devuelve. Se que el tipo de null es Object. Tambien se que 'nada' no es un string porque no tiene las "".
+
+const nada = null;
+console.log(typeof nada); 
