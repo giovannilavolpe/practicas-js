e1cfb071f881e072c3baf20c6a47280d032045b6
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-24 12:36:23 -0300

    feat(script.js) add functions exercices

---
+++ clase05/1/script.js
@@ -0,0 +1,8 @@
+// PARTE A
+
+function areaRectangulo(base, altura) { // Definis la funcion
+    let resultado = base * altura;
+    return resultado; // declaras 'resultado' como el resultado de multiplicar base por altura
+}
+
+console.log(areaRectangulo(9, 9)); //remplazas los valores de base por altura por numeros. En este caso 9 x 9 es 81.
