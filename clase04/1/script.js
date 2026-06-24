0b6332aac54f0c107543eaff605f6f835e57d507
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-24 10:12:55 -0300

    feat(script.js) add loop exercices

---
+++ clase04/1/script.js
@@ -0,0 +1,14 @@
+// PARTE A
+
+ let i = 0;
+ for (let i = 0; i <= 10; i++) {
+     console.log(i);
+}
+
+// PARTE B
+
+let i = 0
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+// Utilice la forma 1, use la misma condicion para que no se pase de 10, i <= 10. Tambien cambie i++ por i += 2 para que sume de a dos.
