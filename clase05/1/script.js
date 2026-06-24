b3d956071cd964a60f0a58abd453cd5602293100
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-24 12:58:24 -0300

    feat(script.js) add part b & part c

--- clase05/1/script.js
+++ clase05/1/script.js
@@ -6,3 +6,27 @@ function areaRectangulo(base, altura) { // Definis la funcion
 }
 
 console.log(areaRectangulo(9, 9)); //remplazas los valores de base por altura por numeros. En este caso 9 x 9 es 81.
+
+
+// PARTE B
+
+function esMayorDeEdad(edad) {
+    if (edad >= 18) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+// esMayorDeEdad(18) devuelve true
+// esMayorDeEdad(17) devuelve false
+
+
+// PARTE C
+
+
+function nombreCompleto(nombre, apellido) {
+    return `Nombre completo: ${nombre} ${apellido}`
+}
+console.log(nombreCompleto("Giovanni", "La Volpe")); // devuelve lo que esta en el return, definimos strings para nombre y para apellido y se muestran en pantalla con el template literal.
