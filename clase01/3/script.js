2d26c473363e4e5f759fe03e7c1ed18325a332e2
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-30 10:50:46 -0300

    feat(script.js) add 3rd exercise

---
+++ clase01/3/script.js
@@ -0,0 +1,18 @@
+// PARTE A
+
+console.log(5 * 3); // pienso que dará 15.
+console.log("5" * 3); // pienso que dará 15.
+console.log("5" + 3); // pienso que dará 53, ya que 5 es un stringy el '+' solo los une.
+console.log(7 % 2); // pienso que devuelve el resto de 7 % 2 (1)
+console.log("Gol" + "azo"); // pienso que devuelve "Golazo" todo junto, porque el + los une.
+console.log("3" - 1); // pienso que dará 2.
+// así fue.
+
+
+
+// PARTE B
+
+console.log("Hola mundo");
+// debe tener "" para que que JS no las tome como variables.
+
+
