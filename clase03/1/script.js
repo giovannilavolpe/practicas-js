de1719385e0d4705d9127fccc3e134d32efd09d9
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-30 11:23:44 -0300

    feat(script.js) add operators exercise

---
+++ clase03/1/script.js
@@ -0,0 +1,46 @@
+//parte a
+
+
+// 7 % 2 pienso que dara el resto de 7 % 2, osea 1
+// 10 % 5 pienso que dara el resto de 10 % 5, osea 0
+// 2 ** 4 pienso que dara 16
+// "5" + 1 pienso que dara 51
+// "5" - 1 pienso que dara 4
+// 5 + "1" pienso que dara 51
+// "5" * "2" pienso que dara 10
+
+console.log(7 % 2,
+10 % 5,
+2 ** 4,
+"5" + 1,
+"5" - 1,
+5 + "1",
+"5" * "2");
+
+
+// parte b
+
+
+// 5 === 5     pienso que dara true
+// 5 === "5"     pienso que dara false (no son iguales)
+// 5 == "5"    pienso que dara true (convierte)
+// 0 == false    pienso que dara true
+// 0 === false     pienso que dara false
+// "" ==     la verdad no se que va a dar. --> Dió true.
+// 3 !== "3"    pienso que dara true
+
+
+console.log(5 === 5,
+5 === "5",
+5 == "5",
+0 == false,
+0 === false,
+"" == false,
+3 !== "3")
+
+
+// parte c
+
+let n = 8
+console.log(n % 2 === 0)
+// esto devuelve true si es par y false si es impar
