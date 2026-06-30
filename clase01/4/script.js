5c4d616e9cbe3515269aa036f46b0f4fe57b336d
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-06-30 10:55:12 -0300

    feat(script.js) add 4th exercise

---
+++ clase01/4/script.js
@@ -0,0 +1,15 @@
+console.table([
+  { equipo: "Argentina", titulos: 3 },
+  { equipo: "Brasil", titulos: 5 },
+  { equipo: "Francia", titulos: 2 },
+  { equipo: "Chile", titulos: 0 },
+]);
+
+console.log([
+  { equipo: "Argentina", titulos: 3 },
+  { equipo: "Brasil", titulos: 5 },
+  { equipo: "Francia", titulos: 2 },
+  { equipo: "Chile", titulos: 0 },
+]);
+
+// la diferencia entre console.log y console.table es que console.table imprime de forma ordenada los elementos, tiene una mejor visualacion. Console.log tambien puede imprimir y ver lo mismo pero para este tipo de casos un console.table viene mejor.
